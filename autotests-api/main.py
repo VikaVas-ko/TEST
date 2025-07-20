75bc54e2d17ab1286602c96cfe53c78ced464af2
VikaVas-ko <psiholognom@gmail.com>
2025-07-20 17:21:08 +0500

    5

--- autotests-api/main.py
+++ autotests-api/main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('H')
+    print_hi('Hy')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
